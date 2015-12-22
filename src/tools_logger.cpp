c1a9e9f8cea9dea7a0f6f4b7a915001cc142674a
Josh Cooper <cooper.s.josh@outlook.com>
2015-12-22 18:21:34 -0400

    revision 2
    rewrote the logger code.
    It is cleaner, smaller, *probably* more efficient too.

--- src/tools_logger.cpp
+++ src/tools_logger.cpp
@@ -1,174 +1,197 @@
 #include "../tools_logger.h"
+#include <cassert>
 
-using namespace dbg_log;
+using namespace logger;
 
-int FileLog_Mgr::StartDelay = 1500;
-int FileLog_Mgr::WriteInterval = 2000;
-bool FileLog_Mgr::ThreadRunning = false;
-std::thread FileLog_Mgr::WtL_Thread;
-
-inline std::string NowTime()
+namespace logger
 {
-	static std::mutex time_mutex;
-	time_mutex.lock();
-	static std::string date_time;
-	static time_t last_raw_time = 0;
-	time_t raw_time = std::chrono::high_resolution_clock::to_time_t( std::chrono::high_resolution_clock::now() );
 
-	if ( raw_time != last_raw_time )
+	inline std::string GetTimeNow()
 	{
-		last_raw_time = raw_time;
-		tm the_time;
-		localtime_s( &the_time, &raw_time );
+		static std::mutex time_mutex;
+		time_mutex.lock();
+		static std::string date_time;
+		static time_t last_raw_time = 0;
+		time_t raw_time = std::chrono::high_resolution_clock::to_time_t( std::chrono::high_resolution_clock::now() );
 
-		char buffer[256];
-		strftime( buffer, sizeof( buffer ), "%Y-%m-%d :: %H:%M:%S", &the_time );
-		date_time = std::string( buffer );
-	}
-
-	static std::string ms;
-	static unsigned long long last_epoch_time = 0;
-	unsigned long long epoch_time = std::chrono::duration_cast<std::chrono::milliseconds>( std::chrono::high_resolution_clock::now().time_since_epoch() ).count();
-
-	if ( epoch_time != last_epoch_time )
-	{
-		last_epoch_time = epoch_time;
-		ms = std::to_string( epoch_time % 1000 );
-	}
-	time_mutex.unlock();
-	return ( date_time + '.' + ms );
-}
-
-std::vector<FileLog*>& FileLog_Mgr::getLogs()
-{
-	static std::vector<FileLog*> Logs;
-	return Logs;
-}
-
-int FileLog_Mgr::RegisterNewLog( std::string file_name, bool thread_safe, bool append, LogLevel reportLevel )
-{
-	static std::mutex registration_mutex;
-	registration_mutex.lock();
-
-	bool original_state = ThreadRunning;
-	if ( ThreadRunning )
-	{
-		Stop();
-	}
-
-	for ( unsigned int i = 0; i < getLogs().size(); ++i )
-	{
-		if ( getLogs()[i]->Output->get_FileName() == file_name )
-			return -2;
-	}
-
-	FileLog* newLog = new FileLog();
-	newLog->ReportingLevel() = reportLevel;
-	newLog->Output->set_append( append );
-	newLog->Output->set_FileName( file_name );
-	newLog->automate_mutex = thread_safe;
-	getLogs().push_back( newLog );
-	unsigned int index = getLogs().size() - 1;
-
-	if ( original_state )
-	{
-		Start();
-	}
-
-	registration_mutex.unlock();
-	return index;
-}
-FileLog FileLog_Mgr::Get( unsigned int index, LogLevel level )
-{
-	auto log = getLogs()[index];
-	return FileLog( log, level );
-}
-
-bool FileLog_Mgr::isThreadSafe( unsigned int index )
-{
-	return getLogs()[index]->isThreadSafe();
-}
-bool FileLog_Mgr::CheckIndex( unsigned int index )
-{
-	if ( index >= 0 && index < getLogs().size() )
-		return true;
-	return false;
-}
-bool FileLog_Mgr::macroCheck( unsigned int index, LogLevel level )
-{
-	if ( CheckIndex( index ) )
-	{
-		if ( level > getLogs()[index]->ReportingLevel() )
-			return false;
-		return true;
-	}
-	return false;
-}
-
-void FileLog_Mgr::WriteToLogs()
-{
-	while ( ThreadRunning )
-	{
-		for ( auto iter = getLogs().begin(); iter != getLogs().end(); ++iter )
+		if ( raw_time != last_raw_time )
 		{
-			auto log = *iter;
-			log->Lock();
-			log->Output->Stream() << log->str();
-			log->flush();
-			log->str( "" );
-			log->Output->Stream().flush();
-			log->Unlock();
+			last_raw_time = raw_time;
+			tm the_time;
+			localtime_s( &the_time, &raw_time );
+
+			char buffer[256];
+			strftime( buffer, sizeof( buffer ), "%Y-%m-%d :: %H:%M:%S", &the_time );
+			date_time = std::string( buffer );
 		}
-		std::this_thread::sleep_for( std::chrono::milliseconds( WriteInterval ) );
+
+		static std::string ms;
+		static unsigned long long last_epoch_time = 0;
+		unsigned long long epoch_time = std::chrono::duration_cast<std::chrono::milliseconds>( std::chrono::high_resolution_clock::now().time_since_epoch() ).count();
+
+		if ( epoch_time != last_epoch_time )
+		{
+			last_epoch_time = epoch_time;
+			ms = std::to_string( epoch_time % 1000 );
+		}
+		time_mutex.unlock();
+		return ( date_time + '.' + ms );
 	}
 
-	//There might be buffered data
-	for ( auto iter = getLogs().begin(); iter != getLogs().end(); ++iter )
+	inline std::string GetLogLevel( LogLevel level )
 	{
-		auto log = *iter;
-		log->Lock();
-		log->Output->Stream() << log->str();
-		log->flush();
-		log->str( "" );
-		log->Output->Stream().flush();
-		log->Unlock();
-	}
-}
-bool FileLog_Mgr::Start()
-{
-	if ( WtL_Thread.joinable() )
-		return false;
+		switch ( level )
+		{
+			case FATAL:
+				return "\t~FATAL~\t\t";
+				break;
 
-	ThreadRunning = true;
-	WtL_Thread = std::thread( &FileLog_Mgr::WriteToLogs );
-	std::this_thread::sleep_for( std::chrono::milliseconds( StartDelay ) );
-	return true;
-}
-bool FileLog_Mgr::Stop()
-{
-	if ( WtL_Thread.joinable() )
-	{
-		ThreadRunning = false;
-		WtL_Thread.join();
-		return true;
+			case ERROR:
+				return "\tERROR: \t\t";
+				break;
+
+			case WARNING:
+				return "WARNING: \t";
+				break;
+
+			case INFO:
+				return "INFO:\t  ";
+				break;
+
+			case DEBUG1:
+				return "DEBUG1:\t\t";
+				break;
+
+			case DEBUG2:
+				return "DEBUG2:\t\t  ";
+				break;
+
+			case DEBUG3:
+				return "DEBUG3:\t\t    ";
+				break;
+
+			case DEBUG4:
+				return "DEBUG4:\t\t      ";
+				break;
+
+			default:
+				return "DoneFucked:\t\t      ";
+		}
 	}
-	return false;
 }
-void FileLog_Mgr::CloseLogs()
+
+
+LogStream::LogStream( Policy* OutputPolicy )
 {
-	Stop();
-	for ( auto iter = getLogs().begin(); iter != getLogs().end(); ++iter )
+	m_OutputPolicy = OutputPolicy;
+}
+
+LogStream::~LogStream()
+{
+	if ( m_OutputPolicy )
 	{
-		auto log = *iter;
-		log->Output->Stream().close();
-		delete log;
+		m_OutputPolicy->lout( this->str() );
 	}
 }
-void FileLog_Mgr::Lock( unsigned int index )
+
+#pragma region "Log"
+
+Log::Log( Policy* OutputPolicy )
 {
-	getLogs()[index]->Lock();
+	assert( OutputPolicy );
+	m_Policy = OutputPolicy;
 }
-void FileLog_Mgr::Unlock( unsigned int index )
+
+Log::~Log()
 {
-	getLogs()[index]->Unlock();
+	delete m_Policy;
 }
+
+LogLevel& Log::ReportingLevel()
+{
+	static LogLevel level;
+	return level;
+}
+
+LogStream Log::Get( LogLevel level )
+{
+	LogStream logStream( m_Policy );
+	logStream << std::endl << " -  " << GetTimeNow() << " - \t" << GetLogLevel( level );
+	return logStream;
+}
+
+#pragma endregion
+
+
+#pragma region "FilePolicy"
+
+bool FilePolicy::Open( std::string FileName, bool Append )
+{
+	m_FileLock.lock();
+	assert( !m_File.is_open() && m_LogFiles.emplace( FileName ).second );
+	m_File.open( FileName, Append ? std::fstream::in | std::fstream::app : std::fstream::in );
+	bool bOpen = m_File.is_open();
+	m_FileLock.unlock();
+	return bOpen;
+}
+
+bool FilePolicy::Close()
+{
+	m_FileLock.lock();
+	m_File.close();
+	bool bClosed = !m_File.is_open();
+	m_FileLock.unlock();
+	return bClosed;
+}
+
+void FilePolicy::lout( const std::string& LogLine )
+{
+	m_FileLock.lock();
+	assert( m_File.is_open() );
+	m_File << LogLine;
+	m_FileLock.unlock();
+}
+
+#pragma endregion
+
+
+#pragma region "LogFile_Manager"
+
+LogFile_Manager::~LogFile_Manager()
+{
+	for ( unsigned int i = 0; i < m_Logs.size(); ++i )
+	{
+		m_Files[i]->Close();
+		delete m_Logs[i];
+	}
+}
+
+LogFile_Manager& LogFile_Manager::Instance()
+{
+	static LogFile_Manager instance;
+	return instance;
+}
+
+unsigned int LogFile_Manager::RegisterLog( std::string FileName, bool Append, LogLevel ReportLevel = DEBUG1 )
+{
+	FilePolicy* file = new FilePolicy();
+	assert( file->Open( FileName, Append ) );
+	Log* log = new Log( file );
+	log->ReportingLevel() = ReportLevel;
+	Instance().m_Files.push_back( file );
+	Instance().m_Logs.push_back( log );
+}
+
+LogStream LogFile_Manager::Get( unsigned int index, LogLevel level )
+{
+	if ( index < Instance().m_Logs.size() )
+	{
+		if ( level <= Instance().m_Logs[index]->ReportingLevel() )
+		{
+			return Instance().m_Logs[index]->Get( level );
+		}
+	}
+	return LogStream( nullptr );
+}
+
+#pragma endregion
--- tools_logger.h
+++ tools_logger.h
@@ -1,24 +1,21 @@
-#ifndef _DBG_LOG_
-#define _DBG_LOG_
+#ifndef _TOOLS_LOGGER_H
+#define _TOOLS_LOGGER_H
 
-#ifndef LOG_MAX_LEVEL
-#define LOG_MAX_LEVEL (dbg_log::FATAL + dbg_log::ERROR + dbg_log::WARNING + dbg_log::INFO + dbg_log::DEBUG1 + dbg_log::DEBUG2 + dbg_log::DEBUG3 + dbg_log::DEBUG4 )
+#ifndef LOG_WRITE_LEVELS
+#define LOG_WRITE_LEVELS (logger::FATAL + logger::ERROR + logger::WARNING + logger::INFO + logger::DEBUG1 + logger::DEBUG2 + logger::DEBUG3 + logger::DEBUG4 )
 #endif
 #pragma comment (lib,"tools_logger.lib")
 
-#include <vector>
-#include <fstream>
-#include <sstream>
-#include <mutex>
-#include <thread>
-
-#include <chrono>
 #include <string>
+#include <sstream>
+#include <fstream>
+#include <mutex>
+#include <set>
+#include <vector>
+#include <thread>
+#include <chrono>
 
-extern inline std::string NowTime();
-
-
-namespace dbg_log
+namespace logger
 {
 	//Enums representing the different log levels
 	// Implemented as bits in a Byte as to facilitate turning specific levels on and off with a #define macro
@@ -30,226 +27,69 @@ namespace dbg_log
 		DEBUG3 = 1 << 6, DEBUG4 = 1 << 7
 	};
 
-	//Forward declaration for friend statement
-	class FileLog_Mgr;
-
-	/*
-	Logger Class
-
-	
-	*/
-	template <typename OutputPolicy>
-	class Logger : public std::ostringstream
+	//class Policy: Ouput Policies' Base Class
+	class Policy
 	{
-		friend FileLog_Mgr;
-	private:
-		bool automate_mutex = false;
-		Logger* original_object = nullptr;
-
-	protected:
-		std::mutex* Log_mutex = nullptr;
-		OutputPolicy* Output = nullptr;  //templated output
-
 	public:
-		virtual ~Logger()
-		{
-			if ( original_object )
-			{
-				( *original_object ) << str();
-				if ( isThreadSafe() )
-					Unlock();
-			}
-			else
-			{
-				delete Log_mutex;
-				delete Output;
-			}
-		}
-		Logger()
-		{
-			original_object = nullptr;
-			Log_mutex = new std::mutex();
-			Output = new OutputPolicy();
-		}
-		Logger( const Logger& obj ) = delete;
-		Logger( Logger* other, LogLevel level )
-		{
-			original_object = other;
-			automate_mutex = other->automate_mutex;
-			Log_mutex = other->Log_mutex;
-			//ReportingLevel() = other.ReportingLevel();
-			if ( isThreadSafe() )
-				Lock();
-			NewLogLine( level );
-		}
-
-		inline LogLevel& ReportingLevel()
-		{
-			static LogLevel reportingLevel = DEBUG4;
-			return reportingLevel;
-		}
-		inline std::string ToString( LogLevel level )
-		{
-			switch ( level )
-			{
-				case FATAL:
-					return "\t~FATAL~\t\t";
-					break;
-
-				case ERROR:
-					return "\tERROR: \t\t";
-					break;
-
-				case WARNING:
-					return "WARNING: \t";
-					break;
-
-				case INFO:
-					return "INFO:\t  ";
-					break;
-
-				case DEBUG1:
-					return "DEBUG1:\t\t";
-					break;
-
-				case DEBUG2:
-					return "DEBUG2:\t\t  ";
-					break;
-
-				case DEBUG3:
-					return "DEBUG3:\t\t    ";
-					break;
-
-				case DEBUG4:
-					return "DEBUG4:\t\t      ";
-					break;
-
-				default:
-					return "DoneFucked:\t\t      ";
-			}
-		}
-		inline void NewLogLine( LogLevel level = INFO )
-		{
-			*this << std::endl << " -  " << NowTime() << " - \t";
-			*this << ToString( level );
-		}
-
-		inline bool isThreadSafe() const { return automate_mutex; }
-		inline void Lock()
-		{
-			Log_mutex->lock();
-		}
-		inline void Unlock()
-		{
-			Log_mutex->unlock();
-		}
-
+		virtual void lout( const std::string& LogLine ) = 0;
 	};
 
-	class FileStream_Policy
-	{
-	public:
-		FileStream_Policy(){}
-
-		inline std::ofstream& Stream()
-		{
-			if ( !file_stream.is_open() )
-			{
-				if ( m_append )
-				{
-					file_stream.open( file_name, std::ofstream::out | std::ofstream::app );
-				}
-				else
-				{
-					file_stream.open( file_name, std::ofstream::out );
-				}
-			}
-			return file_stream;
-		}
-		inline const std::string& get_FileName()
-		{
-			return file_name;
-		}
-		void set_FileName( std::string& file_name )
-		{
-			this->file_name = file_name;
-		}
-		inline void set_append( bool append )
-		{
-			m_append = append;
-		}
-
-	protected:
-		bool m_append = false;
-		std::ofstream file_stream;
-		std::string file_name;
-	};
-
-	//Type Definition for a File Log using Logger<FileStream_Policy>
-	using FileLog = Logger < FileStream_Policy > ;
-
-	class FileLog_Mgr
+	//class LogStream: Core writing mechanic of the Log<T> template class
+	class LogStream : public std::stringstream
 	{
 	private:
-		static bool ThreadRunning;
-		static std::thread WtL_Thread;
-
-	protected:
-		static std::vector<FileLog*>& getLogs();
-		static void WriteToLogs();
-
+		Policy* m_OutputPolicy = nullptr;
 	public:
-		static int WriteInterval;
-		static int StartDelay;
+		LogStream( Policy* OutputPolicy );
+		~LogStream();
+	};
 
-		static int RegisterNewLog( std::string file_name, bool thread_safe = false, bool append = false, LogLevel reportLevel = DEBUG1 );
-		static FileLog Get( unsigned int index, LogLevel level );
+	//class Log: Represents an individual Log... the log's output destination is provided to the log
+	class Log
+	{
+	private:
+		Policy* m_Policy = nullptr;
+	public:
+		Log( Policy* OutputPolicy );
+		~Log();
+		LogLevel& ReportingLevel();
+		LogStream Get( LogLevel level );
+	};
 
-		static bool isThreadSafe( unsigned int index );
-		static bool CheckIndex( unsigned int index );
-		static bool macroCheck( unsigned int index, LogLevel level );
-		
-		static bool Start();
-		static bool Stop();
-		static void CloseLogs();
-		static void Lock( unsigned int index );
-		static void Unlock( unsigned int index );
+	//class FilePolicy: defines how to write logs to a file
+	class FilePolicy : public Policy
+	{
+	private:
+		static std::set<std::string> m_LogFiles;
+		std::string m_FileName;
+		std::mutex m_FileLock;
+		std::fstream m_File;
+	public:
+		bool Open( std::string FileName, bool Append );
+		bool Close();
+		void lout( const std::string& LogLine ) final override;
+	};
 
-
-
+	//class LogFile_Manager: manages log files
+	class LogFile_Manager
+	{
+	private:
+		std::vector<FilePolicy*> m_Files;
+		std::vector<Log*> m_Logs;
+		LogFile_Manager(){}
+		~LogFile_Manager();
+	public:
+		static LogFile_Manager& Instance();
+		static unsigned int RegisterLog( std::string FileName, bool Append, LogLevel ReportLevel = DEBUG1 );
+		static LogStream Get( unsigned int LogIndex, LogLevel level );
 	};
 }
 
-#define LOGFILEX_LOCK( index ) \
-	if ( !dbg_log::FileLog_Mgr::macroCheck( index, level ) ); \
-		else dbg_log::FileLog_Mgr::Lock( index )
-
-#define LOGFILEX_UNLOCK( index, level ) \
-	if ( !dbg_log::FileLog_Mgr::macroCheck( index, level ) ); \) \
-		else dbg_log::FileLog_Mgr::Unlock( index )
-
-#define LOGFILEX( index, level ) \
-	if ( level & ~LOG_MAX_LEVEL || !dbg_log::FileLog_Mgr::macroCheck( index, level ) ); \
-		else dbg_log::FileLog_Mgr::Get( index, level )
-
-#define LOGFILE1(level) \
-	if ( level & ~LOG_MAX_LEVEL || !dbg_log::FileLog_Mgr::macroCheck(0, level) ); \
-		else dbg_log::FileLog_Mgr::Get(0, level)
-
-#define LOGFILE2(level) \
-	if ( level & ~LOG_MAX_LEVEL || !dbg_log::FileLog_Mgr::macroCheck(1, level) ); \
-		else dbg_log::FileLog_Mgr::Get( 1, level )
-
-#define LOGFILE3(level) \
-	if ( level & ~LOG_MAX_LEVEL || !dbg_log::FileLog_Mgr::macroCheck(2, level) ); \
-		else dbg_log::FileLog_Mgr::Get( 2, level )
-
-#define LOGFILE4(level) \
-	if ( level & ~LOG_MAX_LEVEL || !dbg_log::FileLog_Mgr::macroCheck(3, level) ); \
-		else dbg_log::FileLog_Mgr::Get( 3, level )
-
-#define LOGFILE5(level) \
-	if ( level & ~LOG_MAX_LEVEL || !dbg_log::FileLog_Mgr::macroCheck(4, level) ); \
-		else dbg_log::FileLog_Mgr::Get( 4, level )
+#define LOGFILE( index, level )		if ( level & ~LOG_WRITE_LEVELS ); else logger::LogFile_Manager::Get( index, level )
+#define LOGFILE_1( level )				LOGFILE( 0, level )
+#define LOGFILE_2( level )				LOGFILE( 1, level )
+#define LOGFILE_3( level )				LOGFILE( 2, level )
+#define LOGFILE_4( level )				LOGFILE( 3, level )
+#define LOGFILE_5( level )				LOGFILE( 4, level )
 
 #endif
