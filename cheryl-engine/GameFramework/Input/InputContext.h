faac55df1af632ae3e58e055b5aad1d0eb1c2a6b
Josh Cooper <cooper.s.josh@outlook.com>
2016-04-13 20:12:12 -0300

    compiler if guards - bitset union
    MSVC shits the bed if you try to compile a bitset in a union.
    You have to put the bitset inside a struct and union the struct.
    
    GCC is the opposite. It shits the bed if you put the bitset in an
    anonymous struct.
    It is perfectly fine with a bitset in a union otherwise though.
    
    Added if guards to select and compile the correct code for MSVC.

--- cheryl-engine/GameFramework/Input/InputContext.h
+++ cheryl-engine/GameFramework/Input/InputContext.h
@@ -5,9 +5,13 @@
 union InputContext
 {
 	unsigned int value;
-	struct {
+	#ifdef _MSC_VER
+	struct{
 		std::bitset<32> mask;
-	};
+	}
+	#else
+	std::bitset<32> mask;
+	#endif
 
 	InputContext();
 	bool operator==( const InputContext& other );
